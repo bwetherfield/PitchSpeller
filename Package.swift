77b0dfffdffde19c8882e4674683d897fa5daf0d
Ben Wetherfield <bwetherfield@gmail.com>
2019-06-03 18:54:24 -0400

    Change Package name

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "NotationModel",
+    name: "PitchSpeller",
     products: [
         .library(name: "PlotModel", targets: ["PlotModel"]),
         .library(name: "SpelledPitch", targets: ["SpelledPitch"]),
